768c28e176b465d62aea48281cce78c394665133
Koko-John <Koko_jm@icloud.com>
2020-09-09 21:00:53 +1200

    27.Scaffolding a flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(());
+  runApp(MaterialApp(
+    home: Scaffold(
+      //Set the background color of your app home screen
+      backgroundColor: Colors.deepPurple[100],
+        appBar: AppBar(
+            //set the title of the app bar
+            title: Text('I Am Rich'),
+            //set background color of your appBar
+            backgroundColor: Colors.deepPurple,
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage('https://w0.pngwave.com/png/153/633/pink-flowers-purple-violet-purple-flowers-png-clip-art.png')
+          )
+        )
+    )
+  ));
 }
