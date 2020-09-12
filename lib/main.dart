e3aeabde62f5de1aee670b9a4a0b0b6b5e1a6cf9
Koko-John <Koko_jm@icloud.com>
2020-09-12 12:42:55 +1200

    48 tapping into widget properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      //Set the background color of your app home screen
-      backgroundColor: Colors.deepPurple[100],
-        appBar: AppBar(
-            //set the title of the app bar
-            title: Text('I Am Rich'),
-            //set background color of your appBar
-            backgroundColor: Colors.deepPurple,
-        ),
-        body: Center(
-          child: Image(
-              image: NetworkImage('https://w0.pngwave.com/png/153/633/pink-flowers-purple-violet-purple-flowers-png-clip-art.png')
-          )
-        )
-    )
-  ));
+  runApp(MyApp()
+  );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[800],
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.purpleAccent,
+                    backgroundImage: AssetImage('assets/purple.jpg'),
+                  ),
+                  Text(
+                    'Purple',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.white
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
